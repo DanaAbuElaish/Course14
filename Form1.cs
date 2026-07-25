85fefdc3fe75661565ef68a8ad683a854378b3e6
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-25 20:51:47 +0300

    Make button1 ---> disable ---> when click on button3

--- Form1.cs
+++ Form1.cs
@@ -37,6 +37,7 @@ namespace Course14
         private void button3_Click(object sender, EventArgs e)
         {
             textBox1.Enabled = false;
+            button1 .Enabled = false;
         }
 
         private void button4_Click(object sender, EventArgs e)
