5021cf331cf0f61ba54291d12bd0f72c0c28b00e
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-27 12:45:45 +0300

    New Form (Pizza Order) and add label (Make Your Pizza)

---
+++ Pizza Order.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Course14
+{
+    partial class Pizza_Order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbSmall = new System.Windows.Forms.RadioButton();
+            this.rbMeduim = new System.Windows.Forms.RadioButton();
+            this.rbLarge = new System.Windows.Forms.RadioButton();
+            this.lblMakeYourPizza = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rbSmall
+            // 
+            this.rbSmall.AutoSize = true;
+            this.rbSmall.Location = new System.Drawing.Point(39, 159);
+            this.rbSmall.Name = "rbSmall";
+            this.rbSmall.Size = new System.Drawing.Size(62, 20);
+            this.rbSmall.TabIndex = 0;
+            this.rbSmall.TabStop = true;
+            this.rbSmall.Text = "Small";
+            this.rbSmall.UseVisualStyleBackColor = true;
+            // 
+            // rbMeduim
+            // 
+            this.rbMeduim.AutoSize = true;
+            this.rbMeduim.Location = new System.Drawing.Point(39, 185);
+            this.rbMeduim.Name = "rbMeduim";
+            this.rbMeduim.Size = new System.Drawing.Size(76, 20);
+            this.rbMeduim.TabIndex = 1;
+            this.rbMeduim.TabStop = true;
+            this.rbMeduim.Text = "Meduim";
+            this.rbMeduim.UseVisualStyleBackColor = true;
+            // 
+            // rbLarge
+            // 
+            this.rbLarge.AutoSize = true;
+            this.rbLarge.Location = new System.Drawing.Point(39, 211);
+            this.rbLarge.Name = "rbLarge";
+            this.rbLarge.Size = new System.Drawing.Size(63, 20);
+            this.rbLarge.TabIndex = 2;
+            this.rbLarge.TabStop = true;
+            this.rbLarge.Text = "Large";
+            this.rbLarge.UseVisualStyleBackColor = true;
+            // 
+            // lblMakeYourPizza
+            // 
+            this.lblMakeYourPizza.AutoSize = true;
+            this.lblMakeYourPizza.Location = new System.Drawing.Point(205, 15);
+            this.lblMakeYourPizza.Name = "lblMakeYourPizza";
+            this.lblMakeYourPizza.Size = new System.Drawing.Size(107, 16);
+            this.lblMakeYourPizza.TabIndex = 3;
+            this.lblMakeYourPizza.Text = "Make Your Pizza";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(35, 130);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(33, 16);
+            this.lblSize.TabIndex = 4;
+            this.lblSize.Text = "Size";
+            // 
+            // Pizza_Order
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.lblMakeYourPizza);
+            this.Controls.Add(this.rbLarge);
+            this.Controls.Add(this.rbMeduim);
+            this.Controls.Add(this.rbSmall);
+            this.Name = "Pizza_Order";
+            this.Text = "Pizza Order";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbSmall;
+        private System.Windows.Forms.RadioButton rbMeduim;
+        private System.Windows.Forms.RadioButton rbLarge;
+        private System.Windows.Forms.Label lblMakeYourPizza;
+        private System.Windows.Forms.Label lblSize;
+    }
+}
