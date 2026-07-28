dc9588eee7906efcc653ee401fba1d3ac8a6cbb8
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-28 20:39:39 +0300

    Remove Picture

--- Tic-Tac-Toe Game'.Designer.cs
+++ Tic-Tac-Toe Game'.Designer.cs
@@ -44,6 +44,15 @@
             this.labelWinnerName = new System.Windows.Forms.Label();
             this.buttonRestart = new System.Windows.Forms.Button();
             this.labelInProgress = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -58,9 +67,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox1.Location = new System.Drawing.Point(117, 121);
+            this.pictureBox1.Location = new System.Drawing.Point(601, 10);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox1.Size = new System.Drawing.Size(75, 60);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -68,72 +77,72 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox2.Location = new System.Drawing.Point(299, 121);
+            this.pictureBox2.Location = new System.Drawing.Point(698, 13);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox2.Size = new System.Drawing.Size(57, 40);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox3.Location = new System.Drawing.Point(494, 121);
+            this.pictureBox3.Location = new System.Drawing.Point(772, -2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox3.Size = new System.Drawing.Size(60, 40);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox4.Location = new System.Drawing.Point(313, 270);
+            this.pictureBox4.Location = new System.Drawing.Point(698, 76);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox4.Size = new System.Drawing.Size(48, 43);
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox5.Location = new System.Drawing.Point(494, 270);
+            this.pictureBox5.Location = new System.Drawing.Point(772, 35);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox5.Size = new System.Drawing.Size(60, 60);
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox6.Location = new System.Drawing.Point(117, 282);
+            this.pictureBox6.Location = new System.Drawing.Point(628, 76);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox6.Size = new System.Drawing.Size(58, 48);
             this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
             // 
             // pictureBox7
             // 
             this.pictureBox7.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox7.Location = new System.Drawing.Point(494, 455);
+            this.pictureBox7.Location = new System.Drawing.Point(772, 107);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox7.Size = new System.Drawing.Size(67, 30);
             this.pictureBox7.TabIndex = 6;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox8
             // 
             this.pictureBox8.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox8.Location = new System.Drawing.Point(313, 455);
+            this.pictureBox8.Location = new System.Drawing.Point(700, 140);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox8.Size = new System.Drawing.Size(55, 33);
             this.pictureBox8.TabIndex = 7;
             this.pictureBox8.TabStop = false;
             // 
             // pictureBox9
             // 
             this.pictureBox9.Image = global::Course14.Properties.Resources.question_mark_96;
-            this.pictureBox9.Location = new System.Drawing.Point(117, 455);
+            this.pictureBox9.Location = new System.Drawing.Point(628, 140);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(118, 105);
+            this.pictureBox9.Size = new System.Drawing.Size(60, 50);
             this.pictureBox9.TabIndex = 8;
             this.pictureBox9.TabStop = false;
             // 
@@ -153,7 +162,7 @@
             this.labelTurn.AutoSize = true;
             this.labelTurn.Font = new System.Drawing.Font("Microsoft Uighur", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTurn.ForeColor = System.Drawing.Color.Yellow;
-            this.labelTurn.Location = new System.Drawing.Point(760, 121);
+            this.labelTurn.Location = new System.Drawing.Point(781, 140);
             this.labelTurn.Name = "labelTurn";
             this.labelTurn.Size = new System.Drawing.Size(124, 72);
             this.labelTurn.TabIndex = 10;
@@ -195,9 +204,9 @@
             // buttonRestart
             // 
             this.buttonRestart.Font = new System.Drawing.Font("MS Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonRestart.Location = new System.Drawing.Point(761, 488);
+            this.buttonRestart.Location = new System.Drawing.Point(723, 465);
             this.buttonRestart.Name = "buttonRestart";
-            this.buttonRestart.Size = new System.Drawing.Size(178, 55);
+            this.buttonRestart.Size = new System.Drawing.Size(218, 76);
             this.buttonRestart.TabIndex = 14;
             this.buttonRestart.Text = "Restart Game";
             this.buttonRestart.UseVisualStyleBackColor = true;
@@ -214,12 +223,111 @@
             this.labelInProgress.TabIndex = 15;
             this.labelInProgress.Text = "In Prgress";
             // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(117, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 104);
+            this.button1.TabIndex = 16;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(313, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 104);
+            this.button2.TabIndex = 17;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(494, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 104);
+            this.button3.TabIndex = 18;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(117, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 104);
+            this.button4.TabIndex = 19;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(313, 280);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 104);
+            this.button5.TabIndex = 20;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(494, 280);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 104);
+            this.button6.TabIndex = 21;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.Location = new System.Drawing.Point(117, 455);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 104);
+            this.button7.TabIndex = 22;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button8.Location = new System.Drawing.Point(313, 455);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(118, 104);
+            this.button8.TabIndex = 23;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = global::Course14.Properties.Resources.question_mark_96;
+            this.button9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button9.Location = new System.Drawing.Point(494, 455);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(118, 104);
+            this.button9.TabIndex = 24;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
             // Tic_Tac_Toe_Game_
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1031, 600);
+            this.ClientSize = new System.Drawing.Size(1075, 600);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labelInProgress);
             this.Controls.Add(this.buttonRestart);
             this.Controls.Add(this.labelWinnerName);
@@ -271,5 +379,14 @@
         private System.Windows.Forms.Label labelWinnerName;
         private System.Windows.Forms.Button buttonRestart;
         private System.Windows.Forms.Label labelInProgress;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
