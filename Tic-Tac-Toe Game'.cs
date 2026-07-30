8d5b0ae2ac7ef6a8b0a631a182d8c157aa71bf6b
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-07-30 15:27:05 +0300

    Add CheckWinner Funtion

--- Tic-Tac-Toe Game'.cs
+++ Tic-Tac-Toe Game'.cs
@@ -49,9 +49,66 @@ namespace Course14
                 {
                     board[cellNumber] = enCellState.O;
                 }
+                if (CheckWinner())
+                {
+                    return;
+                }
+                SwitchPlayer();
+            }
+
+            
+        }
+
+
+        private void SwitchPlayer()
+        {
+            if (currentPlayer == enPlayers.player1)
+            {
+                currentPlayer = enPlayers.player2;
+            }else
+            {
+                currentPlayer=enPlayers.player1;
             }
         }
 
+
+        private bool CheckWinner ()
+        {
+            if (board[1]!=enCellState.Empty && board[1] == board[2] && board[2] == board[3])
+            {
+                return true;
+            }
+            if (board[4] != enCellState.Empty && board[4] == board[5] && board[5] == board[6])
+            {
+                return true;
+            }
+            if (board[7] != enCellState.Empty && board[7] == board[8] && board[8] == board[9])
+            {
+                return true;
+            }
+            if (board[1] != enCellState.Empty && board[1] == board[5] && board[5] == board[9])
+            {
+                return true;
+            }
+            if (board[3] != enCellState.Empty && board[3] == board[5] && board[5] == board[7])
+            {
+                return true;
+            }
+            if (board[1] != enCellState.Empty && board[1] == board[4] && board[4] == board[7])
+            {
+                return true;
+            }
+            if (board[2] != enCellState.Empty && board[2] == board[5] && board[5] == board[8])
+            {
+                return true;
+            }
+            if (board[3] != enCellState.Empty && board[3] == board[6] && board[6] == board[9])
+            {
+                return true;
+            }
+
+            return false;
+        }
 
         
         private void Tic_Tac_Toe_Game__Paint(object sender, PaintEventArgs e)
